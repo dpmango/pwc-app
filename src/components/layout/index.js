50c062a8cb4d626efbc46df609be90506b0b36e1
Sergey Khmelevskoy <xs290@me.com>
2020-10-17 14:50:27 +0300

    cleanup case sensative folders

--- src/components/layout/index.js
+++
@@ -1,5 +0,0 @@
-import Container from './Container'
-import Head from './Head'
-import Navigation from './Navigation'
-
-export { Container, Head, Navigation }
--- src/components/templates/index.js
+++
@@ -1,3 +0,0 @@
-import StandartTemplate from './StandartTemplate'
-
-export { StandartTemplate }
