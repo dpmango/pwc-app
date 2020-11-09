b52a4376660309313aa22527a995ddcf6854f26a
Sergey Khmelevskoy <xs290@me.com>
2020-11-09 11:37:00 +0300

    remove token code (not used)

--- src/http/constants.js
+++ src/http/constants.js
@@ -1,4 +1,2 @@
-// export const TOKEN = localStorage.getItem('token')
 export const PWC_URL = 'https://ft-pwc-backend.dline-rhost.ru/api'
-export const TOKEN = 'dV2A61Wl7lZYAg7nJni9kgFSLOvjtoXO'
 export const VK_APP_URL = 'https://vk.com/app7631008'
